818bc942fcb8d7fec4ae3f6448fc102c4b9af39a
zstepan <zst330@gmail.com>
2013-07-09 19:01:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require("fs"),
+    sys = require("sys");
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.writeFileSync(index.html));
+  fs.writeFileSync("index.html");
 });
 
 var port = process.env.PORT || 5000;
