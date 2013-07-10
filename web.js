eb5e9aa3ee7ea1abb2f09d505e147d44a2e640e0
zstepan <zst330@gmail.com>
2013-07-10 20:38:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buf = fs.readfileSync('index.html', 'utf8');
+var buf = fs.readFileSync('index.html', 'utf8');
   response.send(buf.toString());
 });
 
