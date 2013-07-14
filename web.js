06cef2b99d75fa9168fdc5cb66211f85502abc38
zstepan <zst330@gmail.com>
2013-07-14 12:26:17 +0000

    Updated

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buf = fs.readFileSync('index.html', 'utf8');
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 console.log("Listening on " + port);
 });
