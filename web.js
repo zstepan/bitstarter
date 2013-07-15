5f1dce6e475023919cfdb5568839af60cd40a02f
zstepan <zst330@gmail.com>
2013-07-15 16:41:34 +0000

    Updated

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var html = "index.html"
+var htmlfile = "index.html"
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var html = fs.readFileSync(html).toString();
+var html = fs.readFileSync(htmlfile).toString();
   response.send(html);
 });
 
